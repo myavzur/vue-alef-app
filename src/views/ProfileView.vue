0ad82179dfdf96bc3bdbbd91ba2e62bc12e6ff89
myavzur <shwave_id404@mail.ru>
2024-08-29 18:23:04 +0300

    Profile page

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
 import ProfileLayout from '@/components/layouts/ProfileLayout/ProfileLayout.vue'
 import Typography from '@/components/ui/Typography'
+import { useUserStore } from '@/stores'
+
+const userStore = useUserStore()
+const userName = userStore.user?.name || 'Неизвестно'
+const userAge = userStore.user?.age || 'Наверное взрослый'
 </script>
 
 <template>
   <ProfileLayout>
-    <div class="about">
-      <Typography type="h-1">This is your profile!</Typography>
+    <div class="info">
+      <Typography type="p-2" weight="medium">Персональные данные</Typography>
+
+      <Typography class="info__name" type="p-2" weight="bold">
+        {{ userName }}, {{ userAge }}
+      </Typography>
     </div>
   </ProfileLayout>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.info {
+  margin-top: 30px;
+
+  &__name {
+    margin-top: 20px;
+  }
+}
+</style>
